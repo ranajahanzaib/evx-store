ea38bc39df33eac15cc4e74674abc9f2640bcf4a
ranajahanzaib <iam@ranajahanzaib.com>
2021-03-08 19:28:54 +0500

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Evx. Store</h1>
+        <p>
+          Online Store | Website Template | Designed with ReactJS &amp; Evvax UI
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
